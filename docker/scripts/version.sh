00f31eadf380d10efc51513ed895b2d94437df07
Roger Lopez <rlopez@localhost.localdomain>
2015-09-10 15:13:47 -0500

    daily

--- docker/scripts/version.sh
+++ docker/scripts/version.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-grep meta /asciidoctor-pdf/data/themes/asciidoctor-no-watermark-theme.yml
+#When using Docker-dev use below:
+#grep meta /asciidoctor-pdf/data/themes/asciidoctor-no-watermark-theme.yml
+
+#When using Docker-stable use below:
+grep meta /usr/local/share/gems/gems/asciidoctor-pdf-1.5.0.alpha.9/data/themes/asciidoctor-no-watermark-theme.yml
